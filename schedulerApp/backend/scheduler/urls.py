69fd7c0a469318a093fcdb1290a420c049629e09
Bruno Freitas <bruno.freitas@neuralmind.ai>
2023-12-04 22:47:56 -0300

    novos updates de organizacao

--- schedulerApp/backend/scheduler/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create', views.AppointmentCreateView.as_view(), name='create'),
-    path('list', views.AppointmentListView.as_view(), name = 'list')
-]
