fb26bb247e745fd57bf103c9cb6a63166720ff13
Bruno Freitas <bruno.freitas@neuralmind.ai>
2023-12-04 22:47:56 -0300

    novos updates de organizacao

--- backend/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
-
-    def __str__(self):
-        return f"User(name={self.name}, email={self.email})"
--- schedulerApp/backend/scheduler/serializers.py
+++
@@ -1,8 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-from .models import Appointment
-
-class AppointmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Appointment
-        fields = ['id', 'start_ts', 'end_ts', 'paciente', 'medico']
