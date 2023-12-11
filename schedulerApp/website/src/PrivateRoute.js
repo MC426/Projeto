9475e57ab5491d28fa916e6f179f5494fb548a3b
Luiz Oda <lhydoda@gmail.com>
2023-12-11 01:45:31 -0300

    troca UserProvider por backendFacade

--- schedulerApp/website/src/PrivateRoute.js
+++ schedulerApp/website/src/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
-import { useUser } from './UserProvider';
+import { useUser } from './backendFacade';
 
 const PrivateRoute = ({ children }) => {
   const { userData, } = useUser();
--- schedulerApp/website/src/index.js
+++ schedulerApp/website/src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserProvider } from './UserProvider';
+import { UserProvider } from './backendFacade';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
